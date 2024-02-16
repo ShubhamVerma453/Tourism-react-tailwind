ae67f3e8412bf4aa3434abcc8ed4db78bff36dcb
shubham_verma <shubham_verma@persistent.com>
2024-02-16 17:41:05 +0530

    Nav design done

--- src/components/First.jsx
+++ src/components/First.jsx
@@ -1,11 +1,18 @@
 import Navbar from "./Navbar";
-import './css/first.css'
 import { IoHeart } from "react-icons/io5";
 
-export default function First(){
+export default function First() {
     return (
-        <div className="first w-screen h-screen text-9xl text-white">
-            hello<IoHeart />
-        </div>
+        <>
+            <div className="first w-screen h-screen bg-cover bg-center">
+                <Navbar />
+                <div className="p-11">
+                    <div>
+                        Welcome to India
+                    </div>
+                </div>
+                
+            </div>
+        </>
     )
 }
