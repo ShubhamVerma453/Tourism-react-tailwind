152972c7327d58202b378465cff00bf18b10719f
shubham_verma <shubham_verma@persistent.com>
2024-02-19 17:00:24 +0530

    first design completed

--- src/components/First.jsx
+++ src/components/First.jsx
@@ -1,21 +1,25 @@
 import Navbar from "./Navbar";
-import { IoHeart } from "react-icons/io5";
 
 export default function First() {
     return (
         <>
             <div className="first w-screen h-screen bg-cover bg-center">
                 <Navbar />
-                <div className="px-11 pt-32 h-[86vh] md:w-3/5 bg-gradient-to-r from-gray-950">
-                    <div className="text-white">
-                        <div className="text-7xl font-[Playfair]">
-                            Welcome to India
+                <div className="text-white px-11 h-[86vh] md:w-3/5 flex items-center bg-gradient-to-r from-gray-950">
+                    <div>
+                        <div className="">
+                            <div className="text-7xl font-[Playfair]">
+                                Welcome to India
+                            </div>
+                            <p>
+                                One of the oldest civilisations in the world, Offers a captivating mosaic of multicultural experiences. From the majestic snow-capped peaks of the Himalayan to the lush tropical rain forests of the south. Immerse yourself in a land with heritage and diverse attractions.
+                            </p>
                         </div>
-                        <p>
-                            One of the oldest civilisations in the world, Offers a captivating mosaic of multicultural experiences. From the majestic snow-capped peaks of the Himalayan to the lush tropical rain forests of the south. Immerse yourself in a land with heritage and diverse attractions.
-                        </p>
+                        <div className="mt-6 flex gap-7">
+                            <button className="py-3 px-8 scale-transi hover:bg-[rgba(255,255,255,.1)] border-2 rounded-md" href="">Guidebook</button>
+                            <button className="py-3 px-8 scale-transi hover:bg-[rgba(255,255,255,.1)] border-2 rounded-md" href="">Plan a Trip</button>
+                        </div>
                     </div>
-
                 </div>
 
             </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,10 +11,10 @@ export default function Navbar(){
             </div>
             <div>
                 <div className="flex gap-4 items-center">
-                <a href="">Destinations</a>
-                <a href="">Things to Do</a>
-                <a href="">Calander</a>
-                <a href=''><img className='w-[40px]' src={map} alt="map" /></a>
+                <a className="scale-transi" href="">Destinations</a>
+                <a className="scale-transi" href="">Things to Do</a>
+                <a className="scale-transi" href="">Calander</a>
+                <a className="scale-transi" href=''><img className='w-[40px]' src={map} alt="map" /></a>
                 </div>
             </div>
         </div>
