598dfbd63cbdd1b00453a2c1fbd478af23533a4a
shubham_verma <shubham_verma@persistent.com>
2024-02-20 11:26:19 +0530

    removing images and add image urls

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import './App.css'
 import First from './components/First'
-import Navbar from './components/Navbar'
+import {tajMahal} from './assets/images.json'
 
 function App() {
-
   return (
     <>
     <First />
+    <img src={tajMahal} alt="new1" />
     </>
   )
 }
